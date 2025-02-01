6667531d82f6cba8b9d546e2239b6fd1c3267b71
Victor <vpanya97@gmail.com>
2025-02-01 17:09:01 +0200

    add raw stars component for rate movies

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; 
-import './index.css'; 
+// import App from './App'; 
+// import './index.css';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating maxRaiting={10} />
+    <StarRating maxRaiting={5} />
+    {/* <App /> */}
   </React.StrictMode>
 );
