e7399994123b33e6e09ce2471ff1e62793b78b18
Victor <vpanya97@gmail.com>
2025-02-01 15:28:14 +0200

    delete some props drillings and create reusable MovieBox component

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,14 +1,113 @@
 import React, { useState } from 'react'
-import WatchedListBox from './WatchedListBox';
-import MovieListBox from './MovieListBox';
+import MovieBox from './MovieBox';
+import tempWatchedData from './tempWatchedData.json';
 
 
-const MainContent = ({movies, setMovies}) => {
+const MainContent = ({movies}) => {
+  const [watched, setWatched] =  useState(tempWatchedData || []);
+
   return (
     <main className="main">
-      <MovieListBox movies={movies} setMovies={setMovies} />
-      <WatchedListBox movies={movies} setMovies={setMovies} />
+      <MovieBox>
+        <MovieList movies={movies} />
+      </MovieBox>
+
+      <MovieBox>
+        <WatchedSumary watched={watched} />
+        <WatchedMovieList watched={watched} />
+      </MovieBox>
     </main>
+  )
+}
+
+const MovieList = ({movies}) => {
+  return (
+      <ul className="list">
+        {movies?.map((movie) => (
+          <Movie key={movie.imdbID} movie={movie} />
+        ))}
+      </ul>
+  )
+    
+}
+
+const Movie = ({movie}) => {
+  return (
+    <li>
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <h3>{movie.Title}</h3>
+      <div>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+      </div>
+    </li>
+  )
+}
+
+const WatchedSumary = ({ watched }) => {
+  const average = (arr) =>
+    arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
+  const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
+  const avgUserRating = average(watched.map((movie) => movie.userRating));
+  const avgRuntime = average(watched.map((movie) => movie.runtime));
+
+  return (
+    <div className="summary">
+      <h2>Movies you watched</h2>
+      <div>
+        <p>
+          <span>#️⃣</span>
+          <span>{watched.length} movies</span>
+        </p>
+        <p>
+          <span>⭐️</span>
+          <span>{avgImdbRating}</span>
+        </p>
+        <p>
+          <span>🌟</span>
+          <span>{avgUserRating}</span>
+        </p>
+        <p>
+          <span>⏳</span>
+          <span>{avgRuntime} min</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+const WatchedMovieList = ({watched}) => {
+  return (
+    <ul className="list">
+      {watched.map((movie) => (
+        <WatchedMovie movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  )
+}
+
+const WatchedMovie = ({movie}) => {
+  return (
+     <li key={movie.imdbID}>
+          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <h3>{movie.Title}</h3>
+          <div>
+            <p>
+              <span>⭐️</span>
+              <span>{movie.imdbRating}</span>
+            </p>
+            <p>
+              <span>🌟</span>
+              <span>{movie.userRating}</span>
+            </p>
+            <p>
+              <span>⏳</span>
+              <span>{movie.runtime} min</span>
+            </p>
+          </div>
+        </li>
   )
 }
 
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = ({ movies, setMovies }) => {
     )
   }
 
-  const NumResult = () => {
+  const NumResult = ({movies}) => {
     return(
       <p className="num-results">
         Found <strong>{movies.length}</strong> results
@@ -36,7 +36,7 @@ const NavBar = ({ movies, setMovies }) => {
     <nav className="nav-bar">
       <Logo />
       <Search />
-      <NumResult />
+      <NumResult movies={movies} />
     </nav>
   )
 }
